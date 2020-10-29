8bf6838881ecf4de5e925935df769f2f0f390c52
Felipe <felipereiss@gmail.com>
2020-10-28 21:01:22 -0300

    FEAT: criação de componentes e renderização deles

--- src/App.js
+++ src/App.js
@@ -1,17 +1,35 @@
 import React, {Component} from "react"
 import ListaNotas from "./components/ListaNotas";
 import FormularioCadastro from "./components/FormularioCadastro";
+import "./assets/App.css"
+import "./assets/index.css"
 
 class App extends Component{
+
+    constructor(props) {
+        super(props);
+        this.notas = [];
+        this.state = {
+            notas:[]
+        }
+    }
+
+    criarNota(titulo, texto){
+        const novaNota = {titulo, texto};
+        const novaArrayNotas = [...this.state.notas, novaNota];
+        const novoEstado = {
+            notas: novaArrayNotas
+        }
+        this.setState(novoEstado);
+    }
     render() {
         return (
-          <section>
-              <FormularioCadastro/>
-              <ListaNotas/>
+          <section className="conteudo">
+              <FormularioCadastro criarNota={this.criarNota.bind(this)}/>
+              <ListaNotas notas={this.state.notas}/>
           </section>
       );
     }
-
 }
 
 export default App;
